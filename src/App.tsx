e7b94a0d16cd4f0cb58883d2bac5296ca1690aed
dianajnxv <dianavorona55@gmail.com>
2026-02-27 16:56:38 +0200

    feat: add base login page structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { MainLayout } from './components/MainLayout/MainLayout';
 import { Home } from './pages/Home/Home';
 import { ROUTES } from './constants';
+import { Login } from './pages/Login/Login';
 import * as Mocks from './pages/Mocks';
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
           <Route path={ROUTES.SHOP} element={<Mocks.Shop />} />
           <Route path={ROUTES.PRODUCT} element={<Mocks.ProductDetails />} />
           <Route path={ROUTES.CONTACT_US} element={<Mocks.ContactUs />} />
-          <Route path={ROUTES.LOGIN} element={<Mocks.Login />} />
+          <Route path={ROUTES.LOGIN} element={<Login />} />
           <Route path={ROUTES.CART} element={<Mocks.Cart />} />
           <Route path="*" element={<Mocks.NotFound />} />
         </Route>
