87ce954d2208a37b5f992fcd0706415cb3163d21
Anna Komar <126970705+annak413@users.noreply.github.com>
2026-03-03 15:37:34 +0200

    feat(56): implement input component (#15)

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { Checkbox } from './Checkbox';
 export { Header } from './Header';
 export * from './LoginForm/LoginForm';
 export { MainLayout } from './MainLayout';
+export { Input } from './Input';
