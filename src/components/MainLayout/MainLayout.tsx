cad6aa223a90790f0530b61fc549ef4d9ca9981c
Vlad <kononvlad007@gmail.com>
2026-03-03 17:52:11 +0200

    feat(59):add header and footer components with theme support

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../Header';
+import { Footer } from '../Footer';
 
 export const MainLayout = () => {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <main className="container mx-auto">
+      <main className="flex-1 bg-gray-50 dark:bg-[#141718]">
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
 };
