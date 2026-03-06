e79640ab4d3be719aaeb529ea75251e740f9d1c0
dianajnxv <dianavorona55@gmail.com>
2026-03-06 10:14:07 +0200

    feat(69): fixed protected route

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -5,10 +5,13 @@ import { ROUTES } from '../../constants';
 import { useAuth } from '../../hooks/useAuth';
 
 export const ProtectedRoute: React.FC = () => {
-  const { isAuth, logout } = useAuth();
+  const { isAuth, role } = useAuth();
 
   if (!isAuth) {
-    logout();
+    return <Navigate to={ROUTES.ADMIN_LOGIN} replace />;
+  }
+
+  if (role !== 'admin') {
     return <Navigate to={ROUTES.ADMIN_LOGIN} replace />;
   }
 
