0e1ad5cc74041f645a34e75c63226b5da443aed9
churight <kchuright@gmail.com>
2026-03-09 20:16:27 +0200

    fix(127): resolving build error

--- src/hooks/useGetAdminProduct.ts
+++ src/hooks/useGetAdminProduct.ts
@@ -1,6 +1,6 @@
 import { useQuery } from '@apollo/client/react';
 
-import { GET_PRODUCT } from '@/services/graphql/productAdminService';
+import { GET_PRODUCTS_PAGE } from '@/services/graphql/productAdminService';
 
 interface GetProductData {
   product: {
@@ -14,7 +14,7 @@ interface GetProductData {
 }
 
 export function useGetAdminProduct(id?: string) {
-  const { data, loading, error } = useQuery<GetProductData>(GET_PRODUCT, {
+  const { data, loading, error } = useQuery<GetProductData>(GET_PRODUCTS_PAGE, {
     variables: { id },
     skip: !id,
   });
