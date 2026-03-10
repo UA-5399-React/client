59cd3c9af4cd30e67fe8c2951d201865db40c45c
Vlad <kononvlad007@gmail.com>
2026-03-10 17:55:28 +0200

    feat(126): add add product button, fixed services

--- src/pages/Admin/Products/AdminProducts.tsx
+++ src/pages/Admin/Products/AdminProducts.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import {
   Button,
@@ -8,7 +9,7 @@ import {
   SortProductsDropdown,
   TableProducts,
 } from '@/components';
-import { DEFAULT_FILTER } from '@/constants';
+import { DEFAULT_FILTER, ROUTES } from '@/constants';
 import { useAdminProducts } from '@/hooks/useAdminProduct';
 import { useDebouncedValue } from '@/hooks/useDebouncedValue';
 import { useDuplicate } from '@/hooks/useDuplicate';
@@ -23,6 +24,7 @@ import { buildSortValue, parseSortValue } from '@/utils/sorting';
 const LIMIT = 10;
 
 export function AdminProducts() {
+  const navigate = useNavigate();
 
   // filters
   const [filters, setFilters] = useState<ProductsFilters>(DEFAULT_FILTER);
@@ -69,63 +71,69 @@ export function AdminProducts() {
     setOrder(nextSort.order);
     setPage(1);
   };
+
   //pagination
   const handlePageChange = (newPage: number) => {
     setPage(newPage);
   };
 
-return (
-  <div>
-    <div className="border-b border-[#CFCFCF] p-5">
-      <h1 className="text-2xl font-bold text-[rgb(var(--color-text))]">
-        Hello, Admin
-      </h1>
-    </div>
+  const handleCreateProduct = () => {
+    navigate(ROUTES.ADMIN_PRODUCT_CREATE);
+  };
 
-    <div className="flex items-center justify-between border-b border-[#e5e7eb] px-4 py-3">
-      <Button
-        variant="outline"
-        onClick={() => setShowFilters((prev) => !prev)}
-        className="flex items-center gap-2 border-gray-300 text-gray-700"
-      >
-        Filters
-      </Button>
-    </div>
-
-    {showFilters && (
-      <div className="border-b border-[#e5e7eb] bg-gray-50 px-4 py-3 text-gray-700">
-        <ProductFiltersBar
-          filters={filters}
-          onChange={handleFiltersChange}
-        />
+  return (
+    <div>
+      <div className="border-b border-[#CFCFCF] p-5">
+        <h1 className="text-2xl font-bold text-[rgb(var(--color-text))]">
+          Hello, Admin
+        </h1>
       </div>
-    )}
 
-    <div className="mx-2 my-5 rounded-l-lg rounded-r-lg border border-[#e5e7eb] pb-4 shadow-md md:mx-5">
-      <div className="flex w-full items-center justify-end gap-4 border-b border-[#e5e7eb] p-4">
-        <SortProductsDropdown
-          value={selectedSortValue}
-          onChange={handleSortChange}
-        />
+      <div className="flex items-center justify-between border-b border-[#e5e7eb] px-4 py-3">
+        <Button
+          variant="outline"
+          onClick={() => setShowFilters((prev) => !prev)}
+          className="flex items-center gap-2 border-gray-300 text-gray-700"
+        >
+          Filters
+        </Button>
 
-        <div className="w-full max-w-[360px]">
-          <SearchInput value={search} onChange={handleSearchChange} />
+        <Button variant="primary" onClick={handleCreateProduct}>
+          + Add Product
+        </Button>
+      </div>
+
+      {showFilters && (
+        <div className="border-b border-[#e5e7eb] bg-gray-50 px-4 py-3 text-gray-700">
+          <ProductFiltersBar filters={filters} onChange={handleFiltersChange} />
         </div>
+      )}
+
+      <div className="mx-2 my-5 rounded-l-lg rounded-r-lg border border-[#e5e7eb] pb-4 shadow-md md:mx-5">
+        <div className="flex w-full items-center justify-end gap-4 border-b border-[#e5e7eb] p-4">
+          <SortProductsDropdown
+            value={selectedSortValue}
+            onChange={handleSortChange}
+          />
+
+          <div className="w-full max-w-[360px]">
+            <SearchInput value={search} onChange={handleSearchChange} />
+          </div>
+        </div>
+
+        <TableProducts
+          items={items}
+          loading={loading}
+          error={error}
+          onDuplicate={duplicateProduct}
+        />
+
+        <Pagination
+          currentPage={page}
+          totalPages={totalPages || 1}
+          onPageChange={handlePageChange}
+        />
       </div>
-
-      <TableProducts
-        items={items}
-        loading={loading}
-        error={error}
-        onDuplicate={duplicateProduct}
-      />
-
-      <Pagination
-        currentPage={page}
-        totalPages={totalPages || 1}
-        onPageChange={handlePageChange}
-      />
     </div>
-  </div>
-);
+  );
 }
