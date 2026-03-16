4d4b37d03d51ba662060f4d9c4b21d58c2fd8a64
churight <kchuright@gmail.com>
2026-03-16 23:49:41 +0200

    feat(150): implemented categories endpoint for filters

---
+++ src/services/graphql/categoryAdminService.ts
@@ -0,0 +1,58 @@
+import { gql } from '@apollo/client';
+
+export const CATEGORY_FIELDS = gql`
+  fragment CategoryFields on CategoryType {
+    id: _id
+    title
+    imageUrl
+    description
+    parent
+    depth
+    createdAt
+    updatedAt
+  }
+`;
+
+export const GET_CATEGORIES_LIST = gql`
+  query GetCategoriesList {
+    categoriesList {
+      ...CategoryFields
+    }
+  }
+  ${CATEGORY_FIELDS}
+`;
+
+export const GET_CATEGORY = gql`
+  query GetCategory($id: ID!) {
+    category(id: $id) {
+      ...CategoryFields
+    }
+  }
+  ${CATEGORY_FIELDS}
+`;
+
+export const CREATE_CATEGORY = gql`
+  mutation CreateCategory($createCategoryInput: CreateCategoryInput!) {
+    createCategory(createCategoryInput: $createCategoryInput) {
+      ...CategoryFields
+    }
+  }
+  ${CATEGORY_FIELDS}
+`;
+
+export const UPDATE_CATEGORY = gql`
+  mutation UpdateCategory($updateCategoryInput: UpdateCategoryInput!) {
+    updateCategory(updateCategoryInput: $updateCategoryInput) {
+      ...CategoryFields
+    }
+  }
+  ${CATEGORY_FIELDS}
+`;
+
+export const DELETE_CATEGORY = gql`
+  mutation DeleteCategory($id: ID!) {
+    deleteCategory(id: $id) {
+      id: _id
+    }
+  }
+`;
