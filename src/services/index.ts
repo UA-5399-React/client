f014949cc3441490c4a6d9377f6c5a9c4871b7d4
pamacat <pavel83838300@gmail.com>
2026-03-18 15:41:55 +0200

    feat(235): integrate product image upload flow on client

--- src/services/index.ts
+++ src/services/index.ts
@@ -12,4 +12,5 @@ export {
   GET_PRODUCTS_PAGE,
 } from './graphql/productAdminService';
 export { productService } from './productService';
+export { uploadService } from './uploadService';
 export { userService } from './userService';
