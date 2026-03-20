f854d5388b2bada8a04e61f27e1995b3f92df0c2
churight <144340708+churight@users.noreply.github.com>
2026-03-20 17:18:39 +0200

    Update authService.test.tsx

--- src/services/authService.test.tsx
+++ src/services/authService.test.tsx
@@ -8,8 +8,6 @@ import {
   vi,
 } from 'vitest';
 
-import { API_BASE_URL } from '@/constants';
-
 import { authService, type LoginPayload } from './authService';
 
 // Change original global fetch for mock-function Vitest
