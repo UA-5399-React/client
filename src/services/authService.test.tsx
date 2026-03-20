f1b5ac1046e0e75460dbf3ef0aa2545b08f90cf9
churight <kchuright@gmail.com>
2026-03-20 14:28:39 +0200

    feat(221): deploy application

--- src/services/authService.test.tsx
+++ src/services/authService.test.tsx
@@ -14,7 +14,7 @@ import { authService, type LoginPayload } from './authService';
 global.fetch = vi.fn();
 
 describe('Service: authService', () => {
-  const API_URL = import.meta.env.BACKEND_URL || 'http://localhost:3000'; // The same default as in service
+  const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'; // The same default as in service
 
   beforeEach(() => {
     // Clear calls and mocks before each test to avoid interference
