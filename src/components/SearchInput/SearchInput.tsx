bbe26e9981fc908c129718967e888dc681c606c5
Gennadiy Greiner <genagreiner94@gmail.com>
2026-03-22 12:29:30 +0200

    fix(225): styles config

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -22,15 +22,14 @@ export function SearchInput({
   const borderClass = clsx({
     'border-gray-600': disabled,
     'border-red-600': error && !disabled,
-    'border-[rgb(var(--default-border))]': !disabled && !error,
+    'border-fieldBorder': !disabled && !error,
   });
 
   // Background and text styles depending on state
   const stateClass = clsx({
-    'bg-gray-200 text-[rgb(var( --color-muted)/0.7)] placeholder:text-[rgb(var(--color-placeholder)/0.7)] cursor-not-allowed':
+    'bg-gray-200 text-muted/70 placeholder:text-placeholderText/70 cursor-not-allowed':
       disabled,
-    'bg-neutral-0 text-[rgb(var( --color-muted))] placeholder:text-[rgb(var(--color-placeholder))]':
-      !disabled,
+    'bg-neutral-0 text-muted placeholder:text-placeholderText': !disabled,
   });
 
   // Focus state
@@ -41,9 +40,9 @@ export function SearchInput({
 
   // Search icon color depending on state
   const iconClass = clsx({
-    'text-[rgb(var(--color-muted)/0.7)]': disabled,
+    'text-muted/70': disabled,
     'text-red-600': error && !disabled,
-    'text-[rgb(var(--color-muted))]': !disabled && !error,
+    'text-muted': !disabled && !error,
   });
 
   return (
