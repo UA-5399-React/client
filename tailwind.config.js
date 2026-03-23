c984bc9d863f416556e524424e0c6a31ca002daa
Gennadiy Greiner <genagreiner94@gmail.com>
2026-03-23 16:40:11 +0200

    fix(225): update theme colors in CSS and Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,43 @@ export default {
         placeholderText: 'rgb(var(--color-placeholder) / <alpha-value>)',
         fieldBorder: 'rgb(var(--default-border) / <alpha-value>)',
         shadowColor: 'rgb(var(--color-shadow) / <alpha-value>)',
+
+        /* red colors */
+        red: {
+          600: 'rgb(var(--color-red-600) / <alpha-value>)',
+          700: 'rgb(var(--color-red-700) / <alpha-value>)',
+        },
+        /* blue colors */
+        blue: {
+          400: 'rgb(var(--color-blue-400) / <alpha-value>)',
+          500: 'rgb(var(--color-blue-500) / <alpha-value>)',
+          800: 'rgb(var(--color-blue-800) / <alpha-value>)',
+        },
+        /* green colors */
+        green: {
+          500: 'rgb(var(--color-green-500) / <alpha-value>)',
+          600: 'rgb(var(--color-green-600) / <alpha-value>)',
+        },
+        /* orange colors */
+        orange: {
+          500: 'rgb(var(--color-orange-500) / <alpha-value>)',
+        },
+        /* gray colors */
+        gray: {
+          50: 'rgb(var(--color-gray-50) / <alpha-value>)',
+          100: 'rgb(var(--color-gray-100) / <alpha-value>)',
+          200: 'rgb(var(--color-gray-200) / <alpha-value>)',
+          300: 'rgb(var(--color-gray-300) / <alpha-value>)',
+          600: 'rgb(var(--color-gray-600) / <alpha-value>)',
+        },
+        /* neutral colors */
+        neutral: {
+          0: 'rgb(var(--color-neutral-0) / <alpha-value>)',
+          50: 'rgb(var(--color-neutral-50) / <alpha-value>)',
+          600: 'rgb(var(--color-neutral-600) / <alpha-value>)',
+          800: 'rgb(var(--color-neutral-800) / <alpha-value>)',
+          900: 'rgb(var(--color-neutral-900) / <alpha-value>)',
+        },
       },
     },
   },
