98aa5265c84b2e883f11f2e9770409c65086d8c2
Vlad <kononvlad007@gmail.com>
2026-03-24 17:27:09 +0200

    fix(193): change test for color variables

--- src/components/OrderCard/OrderProgressBar/OrderProgressBar.test.tsx
+++ src/components/OrderCard/OrderProgressBar/OrderProgressBar.test.tsx
@@ -37,22 +37,15 @@ describe('UI Component: OrderProgressBar', () => {
   it('should render active label text for processed step', () => {
     render(<OrderProgressBar currentStep="processed" />);
 
-    expect(screen.getByText('Processed')).toHaveClass(
-      'text-[rgb(var(--color-text))]',
-    );
-    expect(screen.getByText('En Route')).not.toHaveClass(
-      'text-[rgb(var(--color-text))]',
-    );
+    expect(screen.getByText('Processed')).toHaveClass('text-text');
+
+    expect(screen.getByText('En Route')).not.toHaveClass('text-text');
   });
 
   it('should render active label text for shipped step', () => {
     render(<OrderProgressBar currentStep="shipped" />);
 
-    expect(screen.getByText('En Route')).toHaveClass(
-      'text-[rgb(var(--color-text))]',
-    );
-    expect(screen.getByText('Completed')).not.toHaveClass(
-      'text-[rgb(var(--color-text))]',
-    );
+    expect(screen.getByText('En Route')).toHaveClass('text-text');
+    expect(screen.getByText('Completed')).not.toHaveClass('text-text');
   });
 });
