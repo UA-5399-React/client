83dc4cb05c185b61096207f3ff99d12e10d08d98
Anastasiia <ntkach2006@gmail.com>
2026-03-27 22:26:21 +0200

    feat(292): add delete order from admin page

--- src/components/TableOrders/TableOrders.tsx
+++ src/components/TableOrders/TableOrders.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx';
 
 import { ActionMenu, Dropdown, MainTable } from '@/components';
+import { useDeleteAdminOrder } from '@/hooks/useDeleteAdminOrder';
 import type { Column } from '@/types';
 import { ORDER_STATUS, type OrderItem } from '@/types/tableOrders.types';
 import { capitalizeFirst, formatDate } from '@/utils';
@@ -27,73 +28,96 @@ const statusOptions = Object.values(ORDER_STATUS).map((status) => ({
   value: status,
 }));
 
-const handleEdit = () => {};
+export function TableOrders({ items, loading, error }: TableOrdersProps) {
+  const { deleteOrder } = useDeleteAdminOrder();
 
-const renderProductRow = (item: OrderItem) => {
-  const handleStatusChange = () => {
-    // TODO: handle status change
+  const handleEdit = () => {};
+
+  const handleDelete = async (id: string) => {
+    const confirmed = window.confirm(
+      'Are you sure you want to delete this order?',
+    );
+
+    if (!confirmed) return;
+
+    try {
+      await deleteOrder(id);
+    } catch (error) {
+      console.error('Failed to delete order:', error);
+    }
   };
 
-  const firstProduct = item.items[0];
-  const customerName = `${item.user.firstName} ${item.user.lastName}`.trim();
-  const selectedStatus = item.status ? [String(item.status).toLowerCase()] : [];
+  const renderProductRow = (item: OrderItem) => {
+    const handleStatusChange = () => {
+      // TODO: handle status change
+    };
 
-  return (
-    <>
-      <td>
-        <div className="flex flex-row gap-2">
-          {firstProduct?.imageUrl ? (
-            <img
-              src={firstProduct.imageUrl}
-              alt={firstProduct.title}
-              className="h-10 w-10 rounded object-cover"
-            />
-          ) : null}
-          <div className="flex flex-col gap-1 text-left">
-            <span>{firstProduct?.title ?? 'No items'}</span>
+    const firstProduct = item.items[0];
+    const customerName = `${item.user.firstName} ${item.user.lastName}`.trim();
+    const selectedStatus = item.status
+      ? [String(item.status).toLowerCase()]
+      : [];
 
-            <span> Items: {item.items.length}</span>
+    return (
+      <>
+        <td>
+          <div className="flex flex-row gap-2">
+            {firstProduct?.imageUrl ? (
+              <img
+                src={firstProduct.imageUrl}
+                alt={firstProduct.title}
+                className="h-10 w-10 rounded object-cover"
+              />
+            ) : null}
+
+            <div className="flex flex-col gap-1 text-left">
+              <span>{firstProduct?.title ?? 'No items'}</span>
+              <span>Items: {item.items.length}</span>
+            </div>
           </div>
-        </div>
-      </td>
-      <td>{customerName}</td>
-      <td>#{item.orderId}</td>
-      <td>${item.totalPrice.toFixed(2)}</td>
-      <td>
-        <div className="flex justify-center">
-          <Dropdown
-            label="Status"
-            labelClassName="hidden"
-            options={statusOptions}
-            selectedValues={selectedStatus}
-            onChange={() => handleStatusChange()}
-            multiple={false}
-            hasBorder={true}
-            placeholder="Status"
-            selectClassName={
-              ' ' +
-              clsx(
-                '!flex !items-center !justify-between',
-                '!h-8 !w-[120px] !rounded-[10px] !border !border-[#8F96A3] !bg-white !px-3 !py-0 !text-xs !font-normal !text-[#2C2C2C] !shadow-none hover:!bg-white',
-                '[&_svg]:!h-4 [&_svg]:!w-4 [&_svg]:!text-[#2563EB]',
-              )
-            }
-          />
-        </div>
-      </td>
-      <td>{formatDate(new Date(item.createdAt))}</td>
-      <td>{item.user.phone}</td>
-      <td>
-        <ActionMenu
-          className="top-0 left-[-135px]"
-          editAction={() => handleEdit()}
-        />
-      </td>
-    </>
-  );
-};
+        </td>
 
-export function TableOrders({ items, loading, error }: TableOrdersProps) {
+        <td>{customerName}</td>
+        <td>#{item.orderId}</td>
+        <td>${item.totalPrice.toFixed(2)}</td>
+
+        <td>
+          <div className="flex justify-center">
+            <Dropdown
+              label="Status"
+              labelClassName="hidden"
+              options={statusOptions}
+              selectedValues={selectedStatus}
+              onChange={() => handleStatusChange()}
+              multiple={false}
+              hasBorder={true}
+              placeholder="Status"
+              selectClassName={
+                ' ' +
+                clsx(
+                  '!flex !items-center !justify-between',
+                  '!h-8 !w-[120px] !rounded-[10px] !border !border-[#8F96A3] !bg-white !px-3 !py-0 !text-xs !font-normal !text-[#2C2C2C] !shadow-none hover:!bg-white',
+                  '[&_svg]:!h-4 [&_svg]:!w-4 [&_svg]:!text-[#2563EB]',
+                )
+              }
+            />
+          </div>
+        </td>
+
+        <td>{formatDate(new Date(item.createdAt))}</td>
+        <td>{item.user.phone}</td>
+
+        <td>
+          <ActionMenu
+            className="top-0 left-[-135px]"
+            editAction={() => handleEdit()}
+            deleteAction={() => handleDelete(item.orderId)}
+          />
+        </td>
+      </>
+    );
+  };
+
   return (
     <MainTable
       columns={columns}
