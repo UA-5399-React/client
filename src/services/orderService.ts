ae00a0f4572afe485000ae655aaacf4ac8db7493
Viktor Kushchevoi <victork1609@gmail.com>
2026-04-01 12:51:12 +0300

    feat(304): inegrate my orders page with API

--- src/services/orderService.ts
+++ src/services/orderService.ts
@@ -1,9 +1,14 @@
 import type { CreatedOrder, CreateOrderPayload } from '@/types';
+import type { ApiMyOrder } from '@/types/order.api.types';
 
 import { apiClient } from './api';
 
 export const orderService = {
   createOrder(payload: CreateOrderPayload): Promise<CreatedOrder> {
     return apiClient.post<CreatedOrder>('/orders', payload);
+  },
+
+  getMyOrders(): Promise<ApiMyOrder[]> {
+    return apiClient.get<ApiMyOrder[]>('/orders/my');
   },
 };
