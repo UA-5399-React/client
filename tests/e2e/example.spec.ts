c8fd28fcce7e37631147e4f9705df6a5d53ad519
churight <kchuright@gmail.com>
2026-04-02 16:34:33 +0300

    feat(325): e2e test (playwright) and ci

---
+++ tests/e2e/example.spec.ts
@@ -0,0 +1,54 @@
+import { expect, test } from '@playwright/test';
+
+test('user can browse, filter, add to cart', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await expect(page.getByRole('banner')).toBeVisible();
+
+  await page.getByRole('img', { name: 'Sleek Ceramic Pizza' }).click();
+  await expect(
+    page.getByRole('heading', { name: 'Sleek Ceramic Pizza' }),
+  ).toBeVisible();
+
+  await page.getByRole('button').first().click();
+  await expect(
+    page.getByRole('button', { name: 'Add to Cart' }).first(),
+  ).toBeEnabled();
+
+  await page.getByRole('banner').getByRole('link', { name: 'Shop' }).click();
+  await expect(page).toHaveURL(/\/shop/);
+
+  await page.getByRole('combobox', { name: 'Categories' }).click();
+  await page.getByText('Laptop', { exact: true }).click();
+  await page.getByRole('combobox', { name: 'Price' }).click();
+  await page.getByText('Under $').click();
+  await expect(page.locator('text=Laptop')).toBeVisible();
+
+  await page.getByRole('button', { name: 'Add to Cart' }).first().click();
+  await expect(page.getByRole('button', { name: 'Checkout' })).toBeVisible();
+});
+
+test('user can login', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await expect(page.getByRole('banner')).toBeVisible();
+  await page.getByRole('button', { name: 'User' }).click();
+  await expect(page).toHaveURL(/\/login/);
+  await expect(page.locator('div').nth(4)).toBeVisible();
+  await expect(
+    page.getByRole('img', { name: 'TechnoWorld STORE Logo' }),
+  ).toBeVisible();
+  await page.getByRole('textbox', { name: 'Your email address' }).click();
+  await page
+    .getByRole('textbox', { name: 'Your email address' })
+    .fill('customer@test.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('customer123');
+  await page.getByRole('button', { name: 'Show password' }).click();
+  await expect(
+    page.getByRole('textbox', { name: 'Your email address' }),
+  ).toHaveValue('customer@test.com');
+  await expect(page.getByRole('textbox', { name: 'Password' })).toHaveValue(
+    'customer123',
+  );
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  await expect(page).toHaveURL(/\/shop/);
+});
