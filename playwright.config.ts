fdf4fe1b7f3397bbe2064572e5889c41e635f3e0
churight <kchuright@gmail.com>
2026-04-06 14:14:35 +0300

    feat(325): e2e test (playwright) and ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,13 +5,13 @@ export default defineConfig({
   testMatch: '**/*.spec.ts',
   use: {
     headless: true,
-    baseURL: 'http://localhost:4173', // frontend preview
+    baseURL: 'http://localhost:5173', // frontend preview
     actionTimeout: 20_000,
     navigationTimeout: 30_000,
   },
   webServer: {
-    command: 'pnpm run preview --port 4173',
-    url: 'http://localhost:4173',
+    command: 'pnpm run preview --port 5173',
+    url: 'http://localhost:5173',
     reuseExistingServer: false,
     timeout: 120_000,
   },
