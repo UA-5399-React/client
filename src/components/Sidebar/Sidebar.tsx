0602ee44f7cd7fa256a40a869c0796b7fe177fc6
churight <kchuright@gmail.com>
2026-04-14 22:41:28 +0300

    feat(221): here we go again

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -5,7 +5,7 @@ import {
   LogOut,
   Mail,
   PackageIcon,
-  SettingsIcon,
+  //SettingsIcon,
   ShoppingCartIcon,
   Sparkles,
   UsersIcon,
@@ -29,7 +29,7 @@ const SIDEBAR_LINKS = [
   { to: ROUTES.ADMIN_USERS, label: 'Users', icon: <UsersIcon /> },
   { to: ROUTES.ADMIN_ORDERS, label: 'Orders', icon: <ShoppingCartIcon /> },
   { to: ROUTES.ADMIN_MAILER, label: 'Mailer', icon: <Mail /> },
-  { to: ROUTES.ADMIN_SETTING, label: 'Settings', icon: <SettingsIcon /> },
+  //{ to: ROUTES.ADMIN_SETTING, label: 'Settings', icon: <SettingsIcon /> },
   { to: ROUTES.ADMIN_FEATURED, label: 'Featured Products', icon: <Sparkles /> },
 ];
 
