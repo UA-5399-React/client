21204a2bc79fdd5fa7c9d4ce478fc3348646d0ed
dianajnxv <dianavorona55@gmail.com>
2026-04-28 13:06:52 +0300

    feat(424): fixed tests

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -478,7 +478,7 @@ describe('UI Component: Header', () => {
 
     await user.click(screen.getByRole('button', { name: 'Open menu' }));
 
-    const link = screen.getByLabelText('Wishlist');
+    const link = await screen.findByRole('link', { name: /wishlist/i });
 
     expect(link).toBeInTheDocument();
     expect(link).toHaveAttribute('href', ROUTES.WISHLIST);
