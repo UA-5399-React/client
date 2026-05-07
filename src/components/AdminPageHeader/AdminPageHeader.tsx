1b78bc772311aa2732de6aac329905e1438e2fd8
Mykola Korostelov <askolya23@gmail.com>
2026-05-07 19:22:24 +0300

    fix(475): update AdminPageHeader and Sidebar to display user role dynamically

--- src/components/AdminPageHeader/AdminPageHeader.tsx
+++ src/components/AdminPageHeader/AdminPageHeader.tsx
@@ -1,8 +1,13 @@
+import { useAuth } from '@/hooks/useAuth';
+import { isSuperAdminRole } from '@/utils/permissions';
+
 export const AdminPageHeader = () => {
+  const { role } = useAuth();
+
   return (
     <div className="border-b border-[#CFCFCF] p-5">
       <h1 className="text-2xl font-bold text-[rgb(var(--color-text))]">
-        Hello, Admin 👋
+        Hello, {isSuperAdminRole(role) ? 'Super Admin' : 'Admin'} 👋
       </h1>
     </div>
   );
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -19,7 +19,7 @@ import { ROUTES } from '@/constants';
 import { useAuth } from '@/hooks/useAuth';
 import { useConfirmModal } from '@/hooks/useConfirmModal';
 import { useTheme } from '@/hooks/useTheme';
-import { canAccessAdminRoute } from '@/utils/permissions';
+import { canAccessAdminRoute, isSuperAdminRole } from '@/utils/permissions';
 
 const SIDEBAR_LINKS = [
   { to: ROUTES.ADMIN_DASHBOARD, label: 'Dashboard', icon: <LayoutDashboard /> },
@@ -65,7 +65,7 @@ export const Sidebar = ({ isCollapsed, setIsCollapsed }: SidebarProps) => {
 
   return (
     <aside
-      className={`flex h-full min-h-full flex-col justify-between transition-all duration-300 ${isCollapsed ? 'w-20' : 'w-64'}`}
+      className={`w-full} flex h-full min-h-full flex-col justify-between transition-all duration-300`}
     >
       <div>
         <div
@@ -80,8 +80,8 @@ export const Sidebar = ({ isCollapsed, setIsCollapsed }: SidebarProps) => {
           </button>
         </div>
         {!isCollapsed && (
-          <h1 className="text-text pb-10 text-center text-4xl font-bold">
-            ADMIN
+          <h1 className="text-text pb-5 text-center text-2xl font-bold">
+            {isSuperAdminRole(role) ? 'Super Admin' : 'Admin'}
           </h1>
         )}
 
@@ -113,7 +113,7 @@ export const Sidebar = ({ isCollapsed, setIsCollapsed }: SidebarProps) => {
         onClick={handleLogout}
       >
         <div
-          className={`flex items-center ${isCollapsed ? 'justify-center' : 'gap-2 px-4'} py-8 ${isDark ? 'text-white' : 'text-black'}`}
+          className={`flex items-center ${isCollapsed ? 'justify-center' : 'gap-2 px-4'} text-text py-8 ${isDark ? 'text-white' : 'text-black'}`}
         >
           <LogOut />
           {!isCollapsed && <span>Logout</span>}
