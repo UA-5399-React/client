8c40c4702bc445fd884b9ce3b6728a98b161c1ba
Vlad <kononvlad007@gmail.com>
2026-05-08 18:18:38 +0300

    feat(470): added contact page, Engineer card implemented

---
+++ src/components/EngineerCard/index.ts
@@ -0,0 +1,1 @@
+export { EngineerCard } from './EngineerCard';
--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -108,9 +108,7 @@ describe('UI Component: Header', () => {
     render(<Header />);
     expect(screen.getByRole('link', { name: 'Home' })).toBeInTheDocument();
     expect(screen.getByRole('link', { name: 'Shop' })).toBeInTheDocument();
-    expect(
-      screen.getByRole('link', { name: 'Contact Us' }),
-    ).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'Contact' })).toBeInTheDocument();
   });
 
   it('should have correct href for Shop nav link', () => {
@@ -123,9 +121,9 @@ describe('UI Component: Header', () => {
 
   it('should have correct href for Contact Us nav link', () => {
     render(<Header />);
-    expect(screen.getByRole('link', { name: 'Contact Us' })).toHaveAttribute(
+    expect(screen.getByRole('link', { name: 'Contact' })).toHaveAttribute(
       'href',
-      ROUTES.CONTACT_US,
+      ROUTES.CONTACT,
     );
   });
 
